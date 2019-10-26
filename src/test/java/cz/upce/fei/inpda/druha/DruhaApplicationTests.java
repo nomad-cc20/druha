f8a99a2945cf06d3b30b3dd1c2e152ed5ca91810
nomad <st46619@student.upce.cz>
2019-10-26 10:08:38 +0200

    init

---
+++ src/test/java/cz/upce/fei/inpda/druha/DruhaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.inpda.druha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
