ed8a8354349f736c3be64013876948437029287b
nomad <st46619@student.upce.cz>
2019-10-26 10:12:40 +0200

    init

---
+++ src/main/java/cz/upce/fei/inpda/druha/DruhaApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.inpda.druha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruhaApplication.class, args);
+    }
+
+}
